f0f1c10794f530e7c0a6e6d9e7874d5904ddb028
FGomis <fkgomis@gmail.com>
2021-07-07 11:09:26 +0200

    Create JavaBasicsTarea2.java

---
+++ JavaBasicsTarea2.java
@@ -0,0 +1,23 @@
+
+public class JavaBasicsTarea2 {
+
+	public static void main(String[] args) {
+		
+		int N = 7;
+		double A = 9.9;
+		char C = 'C';
+		
+		System.out.println("Variable N = "+N);
+		
+		System.out.println("Variable A = "+A);
+		
+		System.out.println("Variable C = "+C);
+		
+		System.out.println("La suma de N y A da "+(N+A));
+		
+		System.out.println("La resta de A menos N da "+(A-N));
+		
+		System.out.println("El valor numérico del carácter "+C+" es "+(int)C);
+	}
+	
+}
